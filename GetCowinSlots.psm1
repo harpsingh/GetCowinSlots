40fb0cfce67d232c15bce96efafe6ddc3345b799
harpsingh <harp.singh@gmail.com>
2021-07-27 00:15:00 +0530

    Base commit

---
+++ GetCowinSlots.psm1
@@ -0,0 +1,62 @@
+﻿function GetCowinSlots {
+
+<#
+.SYNOPSIS
+    Prints a list of vaccination slots available in India based on area pin code and number of days to query.
+    It queries public APIs for https://www.cowin.gov.in
+
+.PARAMETER PinCode
+    The 6-digit area pincode
+
+.PARAMETER Days
+    Specifies the number of days to query
+
+.EXAMPLE
+     GetCowinSlots -PinCode 110001 -Days 7
+
+.NOTES
+    Author:  Harprit Singh
+#>
+
+    param (
+        [Parameter(Mandatory)][int]$PinCode,
+        [Parameter(Mandatory)][int]$Days,
+        [bool]$WrapOutput
+    )
+
+    $startDate = $(Get-Date)
+    $endDate = (Get-Date).AddDays($Days)
+    $dateToQuery = $startDate.AddDays(1)
+
+    $slotsCollection = @()
+
+    while ($dateToQuery -le $endDate)
+    {
+        $date = $dateToQuery.ToString("dd-MM-yyyy")
+
+        if ($slots = Invoke-RestMethod -Uri $("https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByPin?pincode=" + $($PinCode) + "&date=$date&min_age_limit=18")){
+            foreach ($slot in $slots.sessions){
+                $slotsCollection += [PSCustomObject]@{
+                    Name	= $slot.name
+                    Address	= $slot.address
+                    Vaccine	= $slot.vaccine
+                    Date	= $slot.date
+                    FeeType	= $slot.fee_type
+                    Fee		= $slot.fee
+                    Slots	= $slot.slots
+                }
+            }
+        }
+        $dateToQuery = $dateToQuery.AddDays(1)
+    }
+
+    if ($slotsCollection){
+        Write-Host "Slots found!!" -ForegroundColor Green
+        $slotsCollection | Format-Table -AutoSize -Wrap
+    }
+    else{
+        Write-Host "No slots found :(" -ForegroundColor Red
+    }
+}
+
+Export-ModuleMember GetCowinSlots
